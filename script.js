5e23f959ea4218616f099967b5612164d52bdf35
ryan hatfield <hatfield086@gmail.com>
2020-08-20 15:12:57 -0500

    Added an event listener for the button to save to local

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ $(document).ready(function () {
     var currentDay = m.format("dddd");
     var currentTime = m.format("h:mm:ss a");
     var currentHour = moment().hours();
+    const block = ["#9", "#10", "#11", "#12", "#1", "#2", "#3", "#4", "#5"];
 
+
+//---Saving the current day and date to the header of the html file.
     $("#currentDay").text(currentDay + ", " + currentDate);
+//---Click event listner to save calendar event to local storage.
+    $(".saveBtn").on("click", function() {
+       var onClick = $(this).attr("data-value");
+       var calEvent = $(onClick).val();
+       localStorage.setItem(onClick, calEvent);
+    });
+
+   
+  
+     
+  
+
+
+
     
 });
